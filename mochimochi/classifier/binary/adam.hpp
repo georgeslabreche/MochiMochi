ae5f9eb13f8b985de170d57f2a30eb13302ea6c7
Georges Labrèche <georges@subspacesignal.com>
2021-03-10 19:47:10 -0500

    make adam serializable

--- mochimochi/classifier/binary/adam.hpp
+++ mochimochi/classifier/binary/adam.hpp
@@ -3,6 +3,13 @@
 
 #include <Eigen/Dense>
 #include <cassert>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <fstream>
 #include "../../functions/enumerate.hpp"
 
 class ADAM {
@@ -44,7 +51,7 @@ public :
     constexpr auto kAlpha = 0.001;
     constexpr auto kBeta1 = 0.9;
     constexpr auto kBeta2 = 0.999;
-    constexpr auto  kEpsilon = 0.00000001;
+    constexpr auto kEpsilon = 0.00000001;
     constexpr auto kLambda = 0.99999999;
 
     if (suffer_loss(feature, label) <= 0.0) { return false; }
@@ -65,8 +72,55 @@ public :
     return true;
   }
 
+  void save(const std::string& filename) {
+    std::ofstream ofs(filename);
+    assert(ofs);
+    boost::archive::text_oarchive oa(ofs);
+    oa << *this;
+    ofs.close();
+  }
+
+  void load(const std::string& filename) {
+    std::ifstream ifs(filename);
+    assert(ifs);
+    boost::archive::text_iarchive ia(ifs);
+    ia >> *this;
+    ifs.close();
+  }
+
   int predict(const Eigen::VectorXd& feature) const {
     return calculate_margin(feature) > 0.0 ? 1 : -1;
+  }
+
+private :
+  friend class boost::serialization::access;
+  BOOST_SERIALIZATION_SPLIT_MEMBER();
+  template <class Archive>
+  void save(Archive& ar, const unsigned int version) const {
+    std::vector<double> w_vector(_w.data(), _w.data() + _w.size());
+    std::vector<double> m_vector(_m.data(), _m.data() + _m.size());
+    std::vector<double> v_vector(_v.data(), _v.data() + _v.size());
+
+    ar & boost::serialization::make_nvp("w", w_vector);
+    ar & boost::serialization::make_nvp("m", m_vector);
+    ar & boost::serialization::make_nvp("v", v_vector);
+    ar & boost::serialization::make_nvp("dimension", const_cast<std::size_t&>(kDim));
+  }
+
+  template <class Archive>
+  void load(Archive& ar, const unsigned int version) {
+    std::vector<double> w_vector;
+    std::vector<double> m_vector;
+    std::vector<double> v_vector;
+
+    ar & boost::serialization::make_nvp("w", w_vector);
+    ar & boost::serialization::make_nvp("m", m_vector);
+    ar & boost::serialization::make_nvp("v", v_vector);
+    ar & boost::serialization::make_nvp("dimension", const_cast<std::size_t&>(kDim));
+
+    _w = Eigen::Map<Eigen::VectorXd>(&w_vector[0], w_vector.size());
+    _m = Eigen::Map<Eigen::VectorXd>(&m_vector[0], m_vector.size());
+    _v = Eigen::Map<Eigen::VectorXd>(&v_vector[0], v_vector.size());
   }
 
 };
